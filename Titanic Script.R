6fc4f67c15aecc8954a8b3f8c383d55efe734c2c
Ioannis Raptis <ioannis_raptis@hotmail.com>
2018-07-30 15:09:02 +0200

    Use two models to take missing age values into account

--- Titanic Script.R
+++ Titanic Script.R
@@ -1,7 +1,6 @@
-# Load the dataset and the SDSFoundations package. 
+# Load the dataset.
 # We added stringsAsFactors=FALSE so as not to create a NULL category in Embarked variable
 dataset_train <- read.csv("data/train.csv", stringsAsFactors=FALSE)
-library("SDSFoundations", lib.loc="C:/Program Files/R/R-3.4.3/library")
 
 # View the column names of the dataset
 names(dataset_train)
@@ -32,6 +31,14 @@ pie(table(dataset_train$Embarked), main = "Embarked Distribution", labels = lbls
 # View the Age distribution
 hist(dataset_train$Age, breaks = 20, main = "Histogram of Age", xlab = "Age")
 
+# Chi-Square, Goodness-of-fit test to determine if rows with missing age
+# follow the population distribution for the Pclass variable
+no_age <- dataset_train[is.na(dataset_train$Age),]
+tbl <- table(no_age$Pclass)
+Expected_proportions <- prop.table(table(dataset_train$Pclass))
+chisq.test(tbl,p=Expected_proportions)$expected # Make sure expected counts > 5
+chisq.test(tbl,p=Expected_proportions) # Turns out we can reject H0
+
 # View the Fare distribution
 hist(dataset_train$Fare, xlab = "Fare", ylab = "Count", main = "Histogram of Fare")
 
@@ -61,7 +68,7 @@ dataset_train_noNULLS$Embarked <- as.factor(dataset_train_noNULLS$Embarked)
 levels(dataset_train_noNULLS$Embarked)
 
 # Create a logistic regression model to predict survival
-mdl <- glm(Survived ~ Pclass + Sex + Age + SibSp + Parch + Fare + HasCabin + Embarked, 
+mdl <- glm(Survived ~ Pclass + Sex + Age + SibSp + Parch + Fare + HasCabin + Embarked,
            data = dataset_train_noNULLS, family = "binomial")
 library(car)
 vif(mdl) # Test for multicolinearity. As expected PClass and HasCabin are somewhat correlated.
@@ -70,22 +77,27 @@ summary(mdl)
 # We use the backwards elimination procedure to refine our model:
 # we eliminate the variable with the highest p > 0.05 and check AIC in every iteration.
 
-# Our final model. 
-mdl <- glm(Survived ~ Pclass + Sex + Age + SibSp, data = dataset_train_noNULLS, family = "binomial")
+# Our final model.
+mdl1 <- glm(Survived ~ Pclass + Sex + Age + SibSp, data = dataset_train_noNULLS, family = "binomial")
 vif(mdl)
 summary(mdl)
 
 # Plot of predicted probabilities of survival vs actual survival
-plot(fitted.values(mdl), dataset_train_noNULLS$Survived, xlab = 'Predicted chance of survival', ylab = 'Actual Survival', 
+plot(fitted.values(mdl1), dataset_train_noNULLS$Survived, xlab = 'Predicted chance of survival', ylab = 'Actual Survival',
      main = 'Plot of predicted probabilities of survival vs actual survival')
 
 # Create survived hat column in our dataset
-dataset_train_noNULLS$prob_of_survival <- fitted.values(mdl)
-dataset_train_noNULLS$survived_hat <- ifelse(fitted.values(mdl) > 0.5, 1, 0)
+dataset_train_noNULLS$prob_of_survival <- fitted.values(mdl1)
+dataset_train_noNULLS$survived_hat <- ifelse(fitted.values(mdl1) > 0.5, 1, 0)
 
 # Create confusion matrix
 confusion_matrix <- table(dataset_train_noNULLS$survived_hat, dataset_train_noNULLS$Survived)
 round(prop.table(confusion_matrix), 2)
+
+#####################################
+# Create a model for data missing the Age value
+dataset_train_noAge <- dataset_train[is.na(dataset_train$Age),]
+mdl2 <- glm(Survived ~ Sex + Parch + HasCabin, data = dataset_train_noAge, family = "binomial")
 
 # Write the new dataset to an excel file, to create our CAP curve.
 # library(openxlsx)
@@ -101,12 +113,19 @@ test_survived_actuals <- read.csv("data/test survived actuals.csv")
 # Convert Pclass to categorical in the test dataset
 dataset_test$Pclass <- as.factor(dataset_test$Pclass)
 
-# Fill out the missing values of age with the mean
-m <- mean(dataset_test$Age,na.rm = T)
-dataset_test$Age <- ifelse(is.na(dataset_test$Age), m,dataset_test$Age)
+# Create the column Cabin to use in our predictions
+dataset_test$HasCabin <- as.factor(ifelse (dataset_test$Cabin != "", "Yes", "No"))
 
-# Use our model to predict survival probabilities.
-dataset_test$p_hat <- predict.glm(mdl,dataset_test,type = "response")
+# Use our models to predict survival probabilities.
+# Split the dataset.
+dataset_test_someAge <- dataset_test[!is.na(dataset_test$Age),]
+dataset_test_noAge <- dataset_test[is.na(dataset_test$Age),]
+# Make predictions.
+dataset_test_someAge$p_hat <- predict.glm(mdl1,dataset_test_someAge,type = "response")
+dataset_test_noAge$p_hat <- predict.glm(mdl2,dataset_test_noAge,type = "response")
+# Merge back the two datasets.
+dataset_test <- rbind(dataset_test_noAge, dataset_test_someAge)
+dataset_test <- dataset_test[order(dataset_test$PassengerId),]
 
 #  Compare predicted to actual survival
 dataset_test$survived <- test_survived_actuals$Survived
